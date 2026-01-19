b677ff94864dde3da14c5b7a951a0979738bf7a7
Narutchai <koonfluk2015@gmail.com>
2026-01-19 15:19:36 +0700

    feat: implement conditional redirect based on authentication status

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { Redirect } from "expo-router";
 
 export default function Index() {
-  return <Redirect href="/(auth)" />;
+  const auth = false;
+  return auth ? <Redirect href="/(tabs)" /> : <Redirect href="/(auth)" />;
 }
