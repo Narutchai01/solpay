a678bb0f246a0304f8f6ed8437cb882324caf1f4
nkmbxy <nakamon22547@gmail.com>
2026-01-27 00:09:21 +0700

    feat: add Assets and Balance components to HomeScreen

---
+++ src/screen/home/assets.component.tsx
@@ -0,0 +1,153 @@
+import { GlassCard } from "@/src/components/card/glass";
+import { Theme } from "@/src/theme/theme";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import {
+    FlatList,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
+
+const ASSETS_DATA = [
+  {
+    id: "1",
+    name: "Solana",
+    sub: "0.10933530228646213 SOL",
+    val: "1,500.00",
+    icon: "logo-bitcoin",
+  },
+  {
+    id: "2",
+    name: "USDC",
+    sub: "0.0312304 USDC",
+    val: "1,000.00",
+    icon: "cash-outline",
+  },
+  {
+    id: "3",
+    name: "USDT",
+    sub: "29.500427134510762 USTD",
+    val: "950.00",
+    icon: "logo-usd",
+  },
+  {
+    id: "4",
+    name: "JupSOL",
+    sub: "0.138711 JUP",
+    val: "550.00",
+    icon: "planet-outline",
+  },
+  {
+    id: "5",
+    name: "PYTH",
+    sub: "14.1651 PYTH",
+    val: "500.00",
+    icon: "pulse-outline",
+  },
+];
+
+const AssetItem = ({ name, sub, val, icon }: any) => (
+  <View style={styles.assetItem}>
+    <View style={styles.assetLeft}>
+      <View style={styles.assetIconBG}>
+        <Ionicons name={icon} style={styles.iconSize} />
+      </View>
+      <View>
+        <Text style={styles.assetNameText}>{name}</Text>
+        <Text style={styles.assetSubText} numberOfLines={1}>
+          {sub}
+        </Text>
+      </View>
+    </View>
+    <Text style={styles.assetValueText}>{val} THB</Text>
+  </View>
+);
+
+export const AssetsComponent = () => {
+  return (
+    <GlassCard style={styles.assetsCard}>
+      <View style={styles.assetsContent}>
+        <View style={styles.assetsHeader}>
+          <Text style={styles.assetsTitle}>Assets</Text>
+          <TouchableOpacity>
+            <Text style={styles.viewAllText}>View All</Text>
+          </TouchableOpacity>
+        </View>
+
+        <FlatList
+          data={ASSETS_DATA}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <AssetItem
+              name={item.name}
+              sub={item.sub}
+              val={item.val}
+              icon={item.icon as any}
+            />
+          )}
+          scrollEnabled={false}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </GlassCard>
+  );
+};
+
+const styles = StyleSheet.create({
+  assetsCard: { marginTop: 32 },
+  assetsContent: { padding: 20, paddingBottom: 10 },
+  assetsHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  assetsTitle: {
+    color: Theme.colors.surface,
+    fontSize: Theme.fontSize.h5,
+    fontWeight: "bold",
+  },
+  viewAllText: {
+    color: Theme.colors.surface,
+    fontSize: Theme.fontSize.textS,
+    textDecorationLine: "underline",
+  },
+  assetItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  assetLeft: { flexDirection: "row", alignItems: "center", flex: 1 },
+  assetIconBG: {
+    width: 44,
+    height: 44,
+    borderRadius: 22,
+    backgroundColor: Theme.colors.g300,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 12,
+  },
+  assetNameText: {
+    color: Theme.colors.surface,
+    fontSize: Theme.fontSize.h7,
+    fontWeight: "medium",
+  },
+  assetSubText: {
+    color: Theme.colors.g50,
+    fontSize: Theme.fontSize.textS,
+    marginTop: 2,
+    maxWidth: 200,
+  },
+  assetValueText: {
+    color: Theme.colors.surface,
+    fontSize: Theme.fontSize.h7,
+    fontWeight: "medium",
+  },
+  iconSize: {
+    fontSize: 24,
+    color: Theme.colors.surface,
+  },
+});
