dd29658b6a5ba0c8dd83c7ec496008636a1eed29
nkmbxy <nakamon22547@gmail.com>
2026-02-13 22:37:51 +0700

    feat: add SuccessLayout component and refactor SwapSuccessScreen to utilize it

--- src/screen/swap/swapSuccess.screen.tsx
+++ src/screen/swap/swapSuccess.screen.tsx
@@ -1,98 +1,23 @@
-import { Button } from "@/src/components/button/button";
-import { GlassCard } from "@/src/components/card/glass";
-import GradientLayout from "@/src/components/shard/gradieintLayout";
-import { Theme } from "@/src/theme/theme";
+import {
+  SuccessDetailItem,
+  SuccessLayout,
+} from "@/src/components/shard/successLayout";
 import { router } from "expo-router";
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 export const SwapSuccessScreen = () => {
-  const DetailRow = ({ label, value }: { label: string; value: string }) => (
-    <View style={styles.row}>
-      <Text style={styles.rowLabel}>{label}</Text>
-      <Text style={styles.rowValue}>{value}</Text>
-    </View>
-  );
+  const swapData: SuccessDetailItem[] = [
+    { label: "Transaction Id", value: "11111111" },
+    { label: "Exchange Rate", value: "1 USDT ≈ 0.0072 SOL" },
+    { label: "Fee", value: "0.00 THB" },
+    { label: "SOL Amount", value: "2.50 SOL" },
+    { label: "USDT Amount", value: "345.16312 USDT" },
+    { label: "Completion date", value: "27 Nov 2025 - 18:43" },
+  ];
 
   return (
-    <GradientLayout>
-      <SafeAreaView style={styles.safeArea}>
-        <View style={styles.container}>
-          {/* Success Icon */}
-          <Image
-            source={require("@/assets/images/success-icon.png")}
-            style={styles.successIcon}
-          />
-
-          {/* Detail Card */}
-          <GlassCard style={styles.detailCard}>
-            <DetailRow label="Transaction Id" value="11111111" />
-            <DetailRow label="Exchange Rate" value="1 USDT ≈ 0.0072 SOL" />
-            <DetailRow label="Fee" value="0.00 THB" />
-            <DetailRow label="SOL Amount" value="2.50 SOL" />
-            <DetailRow label="USDT Amount" value="345.16312 USDT" />
-            <DetailRow label="Completion date" value="27 Nov 2025 - 18:43" />
-          </GlassCard>
-        </View>
-
-        {/* Button */}
-        <View style={styles.button}>
-          <Button
-            title="Done"
-            variant="solid"
-            color="v300"
-            onPress={() => router.replace("/(tabs)")}
-            style={styles.doneButton}
-            textColor="g300"
-          />
-        </View>
-      </SafeAreaView>
-    </GradientLayout>
+    <SuccessLayout
+      details={swapData}
+      onButtonPress={() => router.replace("/(tabs)")}
+    />
   );
 };
-
-const styles = StyleSheet.create({
-  safeArea: { flex: 1 },
-  container: {
-    flex: 1,
-    paddingHorizontal: 16,
-    alignItems: "center",
-  },
-  // Icon
-  successIcon: {
-    width: 125,
-    height: 103,
-    justifyContent: "center",
-    alignItems: "center",
-    marginBottom: 32,
-  },
-  // Card
-  detailCard: {
-    width: "100%",
-    paddingHorizontal: 12,
-  },
-  row: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingVertical: 12,
-  },
-  rowLabel: {
-    color: Theme.colors.g50,
-    fontSize: Theme.fontSize.h6,
-    fontWeight: "500",
-  },
-  rowValue: {
-    color: Theme.colors.g50,
-    fontSize: Theme.fontSize.h6,
-    fontWeight: "500",
-  },
-  // Button
-  button: {
-    paddingHorizontal: 16,
-    paddingBottom: 20,
-  },
-  doneButton: {
-    paddingVertical: 10,
-  },
-});
