cceeaaa71d56d866c61a43cf66097ab1af69a2a8
nkmbxy <nakamon22547@gmail.com>
2026-02-26 23:44:48 +0700

    feat: add ProfileScreen and update layout components for profile and transfer

--- app/profile/_layout.tsx
+++ app/profile/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 
-export default function ProfileLayout() {
+export default function TransferVerifyInformationLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
