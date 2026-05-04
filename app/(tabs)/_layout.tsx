6e656660b0c77cbf3b36501c3b72f0bfb0f710db
nkmbxy <nakamon22547@gmail.com>
2026-05-04 19:53:10 +0700

    Refactor TabsLayout to enhance tabBar styling and add icons for each tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,62 @@
+import { Theme } from "@/src/core/theme/theme";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="topup" options={{ title: "Top up" }} />
-      <Tabs.Screen name="pay" options={{ title: "Pay" }} />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: Theme.colors.g300,
+          borderTopWidth: 0,
+          elevation: 0,
+          shadowOpacity: 0,
+        },
+        tabBarActiveTintColor: Theme.colors.v300,
+        tabBarInactiveTintColor: Theme.colors.g50,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="topup"
+        options={{
+          title: "Top up",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="wallet" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="pay"
+        options={{
+          title: "Pay",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="line-scan"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="restore" size={size} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
